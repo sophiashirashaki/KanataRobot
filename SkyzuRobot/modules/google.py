dea250a5c4af60d98d8e7893543ece1f3e5e3f35
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-12-05 18:30:52 +0800

    Update google.py

--- SkyzuRobot/modules/google.py
+++ SkyzuRobot/modules/google.py
@@ -268,7 +268,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Skyzu <==="
+        app_details += "\n\n===> Kanata <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
