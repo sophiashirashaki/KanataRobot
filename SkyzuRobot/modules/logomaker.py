0fb8ff3850295c4b6cb277f8f17ba9d643e009f0
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-12-05 18:19:31 +0800

    Update logomaker.py

--- SkyzuRobot/modules/logomaker.py
+++ SkyzuRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [Kanata](https://t.me/Kanatapro_bot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [Kanata](https://t.me/Kanatapro_bot)",
             force_document=False,
         )
         os.remove(flnme)
