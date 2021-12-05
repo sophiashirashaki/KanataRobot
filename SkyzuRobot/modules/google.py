0be3cea23bfb1b1d03ebf3971a5c4b2f33947911
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-12-05 18:15:07 +0800

    Update google.py

--- SkyzuRobot/modules/google.py
+++ SkyzuRobot/modules/google.py
@@ -287,7 +287,7 @@ __help__ = """
 ❂ /github <username>*:* Get information about a GitHub user.
 ❂ /country <country name>*:* Gathering info about given country
 ❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Skyzu <query>*:* Skyzu answers the query
+❂ Kanata <query>*:* Kanata answers the query
 
-  💡Ex: `Skyzu where is Japan?`
+  💡Ex: `Kanata where is Japan?`
 """
