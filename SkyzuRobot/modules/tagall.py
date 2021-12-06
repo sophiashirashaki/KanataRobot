63214df3f5b35770151041f38f9eac4f0b93147b
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-12-06 08:31:47 +0800

    Update tagall.py

--- SkyzuRobot/modules/tagall.py
+++ SkyzuRobot/modules/tagall.py
@@ -9,7 +9,7 @@ from SkyzuRobot.events import register as tomori
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "Hi Friends I'm Skyzu I Call To All Of You"
+    mentions = "Hi Friends I'm Kanata I Call To All Of You"
     chat = await event.get_input_chat()
     async for x in telethn.iter_participants(chat, 100):
         mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
