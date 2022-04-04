58084267ce7c88644a936667d909b94854651d35
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2022-04-04 17:25:02 +0800

    Update __init__.py

--- SkyzuRobot/__init__.py
+++ SkyzuRobot/__init__.py
@@ -298,5 +298,5 @@ from SkyzuRobot.modules.helper_funcs.handlers import (
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler"
+tg.MessageHandler = CustomMessageHandler
  https://raw.githubusercontent.com/Skyzu/SkyzuRobot/main/SkyzuRobot/__init__.py#:~:text=import%20logging%0Aimport,tg.MessageHandler%20%3D%20CustomMessageHandler
